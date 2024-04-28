bfda55adf4b54db0ae3be2be762d6f83874aa641
Nicholas Devenish <ndevenish@gmail.com>
2024-04-28 19:32:46 +0100

    Add Webb bases
    As per https://makerworld.com/en/models/446624

---
+++ src/genlabel/bases/webb.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+from build123d import (
+    Axis,
+    BuildLine,
+    BuildPart,
+    BuildSketch,
+    Mode,
+    Plane,
+    Polyline,
+    Rectangle,
+    Vector,
+    extrude,
+    fillet,
+    make_face,
+)
+
+from . import LabelBase
+
+
+def body() -> LabelBase:
+    """
+    Generate a Webb-style label body
+
+    Origin is positioned at the center of the label, with the label
+    surface at z=0.
+
+    Returns:
+        A LabelBase tuple consisting of:
+            part: The actual label body.
+            label_area: A vector describing the width, height of the usable area.
+    """
+    width = 36.4
+    height = 11
+    depth = 1
+    with BuildPart() as part:
+        with BuildSketch():
+            Rectangle(width=width, height=height)
+
+        extrude(amount=-depth)
+        fillet(part.edges().filter_by(Axis.Z), radius=0.5)
+
+        # Plane.XZ
+        with BuildSketch(Plane.XZ) as _sketch:
+            for x in [-12.133, 0, 12.133]:
+                with BuildLine(Plane.XZ) as _line:
+                    Polyline(
+                        [
+                            (x - 0.75, -0.4),
+                            (x - 0.5, -1),
+                            (x + 0.5, -1),
+                            (x + 0.75, -0.4),
+                        ],
+                        close=True,
+                    )
+                make_face()
+        extrude(amount=height / 2, both=True, mode=Mode.SUBTRACT)
+
+    return LabelBase(part.part, Vector(36.4, 11))
--- src/genlabel/cli.py
+++ src/genlabel/cli.py
@@ -32,7 +32,7 @@ from build123d import (
 )
 
 from . import fragments
-from .bases import plain, pred
+from .bases import plain, pred, webb
 from .label import render_divided_label
 from .options import LabelStyle, RenderOptions
 
@@ -82,7 +82,7 @@ def run(argv: list[str] | None = None):
     parser = ArgumentParser(description="Generate pred-style gridfinity bin labels")
     parser.add_argument(
         "--base",
-        choices=["pred", "plain", "none"],
+        choices=["pred", "plain", "none", "webb"],
         default="pred",
         help="Label base to generate onto. [Default: %(default)s]",
     )
@@ -180,7 +180,7 @@ def run(argv: list[str] | None = None):
         args.labels = ["{webbolt(pozi)}{...}M3×20"]
 
     if not args.width:
-        if args.base == "pred":
+        if args.base in {"pred", "webb"}:
             args.width = "1"
         else:
             sys.exit(f"Error: Must specify width for label base '{args.base}'.")
@@ -205,6 +205,8 @@ def run(argv: list[str] | None = None):
                             f"Warning: Small width ({args.width}) for plain base. Did you specify in mm?"
                         )
                     body = plain.body(args.width, args.height)
+                elif args.base == "webb":
+                    body = webb.body()
                 else:
                     body = None
 
