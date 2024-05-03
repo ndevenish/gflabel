6b59560926205f028cbe35a17e92c78fe5c9c407
Nicholas Devenish <ndevenish@gmail.com>
2024-05-03 17:17:33 +0100

    Update webb base to v1.1.0 style
    The trapezoids have been changed to slightly easier to
    print curved profiles.

--- src/gflabel/bases/webb.py
+++ src/gflabel/bases/webb.py
@@ -1,15 +1,20 @@
 from __future__ import annotations
 
 from build123d import (
+    Axis,
     BuildLine,
     BuildPart,
     BuildSketch,
+    Edge,
     Mode,
     Plane,
     Polyline,
     RectangleRounded,
+    ShapeList,
     Vector,
+    chamfer,
     extrude,
+    fillet,
     make_face,
 )
 
@@ -40,17 +45,73 @@ def body() -> LabelBase:
         # Plane.XZ
         with BuildSketch(Plane.XZ) as _sketch:
             for x in [-12.133, 0, 12.133]:
-                with BuildLine(Plane.XZ) as _line:
+                with BuildLine() as _line:
                     Polyline(
                         [
-                            (x - 0.75, -0.4),
                             (x - 0.5, -1),
+                            (x - 0.5, -0.8),
+                            (x - 1, -0.8),
+                            (x - 1, -0.4),
+                            (x + 1, -0.4),
+                            (x + 1, -0.8),
+                            (x + 0.5, -0.8),
                             (x + 0.5, -1),
-                            (x + 0.75, -0.4),
                         ],
                         close=True,
                     )
+                    # mirror(_line.line, Plane.YZ)
                 make_face()
         extrude(amount=height / 2, both=True, mode=Mode.SUBTRACT)
+
+        # Now, handle v1.1.0 fillets/chamfers
+        verts = (
+            part.edges()
+            .filter_by(Axis.Z)
+            .filter_by(lambda x: x.length < 1)
+            .group_by(lambda x: x.length)
+        )
+        # This needs to be done in two passes in build123d, for some reason
+        fillet(verts[0], radius=0.5)
+        fillet(verts[1], radius=0.5)
+
+        # Now, find the edges to chamber
+
+        def _get_all_start_edges():
+            """Extracts the two Y-axis inner lines for each channel"""
+            # Get a list of all edges we want to consider
+            all_candidates = (
+                part.edges().filter_by(Axis.Y).filter_by_position(Axis.Z, -0.85, -0.75)
+            )
+            # Now let's select the inner Y-axis edges that we know we want
+            start_edges = ShapeList()
+            for x in [-12.133, 0, 12.133]:
+                start_edges.extend(
+                    all_candidates.filter_by_position(Axis.X, x - 0.55, x + 0.55)
+                )
+            return start_edges
+
+        # Now we follow each of these edges
+        def _edge_matcher(vertex_set: ShapeList):
+            """Given a list of vertices, returns a matcher for edges touching them"""
+            vs = vertex_set.vertices()
+
+            def _match_edge(edge: Edge) -> bool:
+                for v in edge.vertices():
+                    for v2 in vs:
+                        if v.distance_to(v2) < 0.001:
+                            return True
+                return False
+
+            return _match_edge
+
+        cands_to_chamfer = (
+            part.edges().filter_by(Plane.XY).filter_by_position(Axis.Z, -0.85, -0.75)
+        )
+        # Now, expand the selection twice, following all edges touching the known good edges
+        ext_1 = cands_to_chamfer.filter_by(_edge_matcher(_get_all_start_edges()))
+        ext_2 = cands_to_chamfer.filter_by(_edge_matcher(ext_1))
+
+        # Finally, Chamfer these. We want 0.2/0.1 but 100% chamfers seem not to work well.
+        chamfer(ext_2, length=0.1999, length2=0.1)
 
     return LabelBase(part.part, Vector(36.4, 11))
