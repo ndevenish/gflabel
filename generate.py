5895e9d4f80050e7965322c63a64b3455edbe4f2
Nicholas Devenish <ndevenish@gmail.com>
2024-05-08 23:15:11 +0100

    Make generator capable of selective running

--- generate.py
+++ generate.py
@@ -4,98 +4,140 @@ Generate the example layouts for the README
 
 from __future__ import annotations
 
+import argparse
 import subprocess
-import sys
 
 import gflabel.fragments as fragments
 
-vscode = ["--vscode"] if "--vscode" in sys.argv else []
-EXAMPLES = [
-    "L{...}R",
-    "{box(35)}",
-]
+parser = argparse.ArgumentParser(description="Generate example images for GFlabel")
+parser.add_argument("--vscode", help="Run in OCP_vscode mode", action="store_true")
+parser.add_argument("--fragment", help="Run the fragment examples", action="store_true")
+parser.add_argument("--bolt", help="Run the bolt examples", action="store_true")
+parser.add_argument("--drives", help="Run the drive examples", action="store_true")
+parser.add_argument("--symbols", help="Run the symbol examples", action="store_true")
 
-HEAD = "gflabel --base=none -w=100 --height=12 --divisions=3 --vscode"
+args = parser.parse_args()
+vscode = ["--vscode"] if args.vscode else []
+if all(not x for x in [args.fragment, args.bolt, args.drives, args.symbols]):
+    args.fragment = True
+    args.bolt = True
+    args.drives = True
+    args.symbols = True
 
-command = [
-    "gflabel",
-    "--base=none",
-    "-w=100",
-    "--height=12",
-    "--divisions=3",
-    "-o",
-    "examples.svg",
-    *vscode,
-    "--font-size=7",
-]
+if args.fragment:
+    EXAMPLES = [
+        "L{...}R",
+        "{box(35)}",
+    ]
 
-for frag in fragments.fragment_description_table():
-    for example in frag.examples:
-        print(frag.names[0], example)
-        command.extend([example.replace("{", "{{").replace("}", "}}"), "", example])
+    HEAD = "gflabel --base=none -w=100 --height=12 --divisions=3 --vscode"
 
-subprocess.run(command)
-# sys.exit(1)
+    command = [
+        "gflabel",
+        "--base=none",
+        "-w=100",
+        "--height=12",
+        "--divisions=3",
+        "-o",
+        "examples.svg",
+        *vscode,
+        "--font-size=7",
+    ]
 
-# Generate a table of head drive types
-command = [
-    "gflabel",
-    "--base=none",
-    "-w=100",
-    "--height=12",
-    "--divisions=4",
-    "--font-size=5",
-    "-o",
-    "drives.svg",
-    *vscode,
-    # "{{head(DRIVE)}}",
-    # "",
-    # "",
-    # "",
-]
-drives = [x for x in sorted(fragments.DRIVES) if x != "security"]
-drives.extend(["slot,triangle", "slot,square", "torx,security"])
-for drive in drives:
-    command.extend([f"{{...}}{drive}", f"{{head({drive})}}"])
-subprocess.run(command)
+    for frag in fragments.fragment_description_table():
+        for example in frag.examples:
+            print(frag.names[0], example)
+            command.extend([example.replace("{", "{{").replace("}", "}}"), "", example])
 
-# sys.exit(1)
+    subprocess.run(command)
 
-# And bolt styles
-command = [
-    "gflabel",
-    "--base=none",
-    "-w=100",
-    "--height=12",
-    "--divisions=3",
-    "--font-size=5",
-    "-o",
-    "bolts.svg",
-    *vscode,
-    "bolt(10,",
-    "Style",
-    "webbolt",
-]
+if args.drives:
+    # Generate a table of head drive types
+    command = [
+        "gflabel",
+        "--base=none",
+        "-w=100",
+        "--height=12",
+        "--divisions=4",
+        "--font-size=5",
+        "-o",
+        "drives.svg",
+        *vscode,
+        # "{{head(DRIVE)}}",
+        # "",
+        # "",
+        # "",
+    ]
+    drives = [x for x in sorted(fragments.DRIVES) if x != "security"]
+    drives.extend(["slot,triangle", "slot,square", "torx,security"])
+    for drive in drives:
+        command.extend([f"{{...}}{drive}", f"{{head({drive})}}"])
+    subprocess.run(command)
 
-for style in [
-    ["pan"],
-    ["socket"],
-    ["round"],
-    ["countersunk"],
-    ["tapping"],
-    ["flipped"],
-]:
-    text = ",".join(style)
-    command.extend([f"{{bolt(10,{text})}}", text, f"{{webbolt({text})}}"])
+if args.bolt:
+    # And bolt styles
+    command = [
+        "gflabel",
+        "--base=none",
+        "-w=100",
+        "--height=12",
+        "--divisions=3",
+        "--font-size=5",
+        "-o",
+        "bolts.svg",
+        *vscode,
+        "bolt(10,",
+        "Style",
+        "webbolt",
+    ]
 
-# Bolt-only
-for style in [["slot"], ["pan,flanged"]]:
-    text = ",".join(style)
-    command.extend([f"{{bolt(10,{text})}}", text, ""])
+    for style in [
+        ["pan"],
+        ["socket"],
+        ["round"],
+        ["countersunk"],
+        ["tapping"],
+        ["flipped"],
+    ]:
+        text = ",".join(style)
+        command.extend([f"{{bolt(10,{text})}}", text, f"{{webbolt({text})}}"])
 
-# Webb-only
-for style in [["partial"], ["hex", "security"]]:
-    text = ",".join(style)
-    command.extend(["", text, f"{{webbolt({text})}}"])
+    # Bolt-only
+    for style in [["slot"], ["pan,flanged"]]:
+        text = ",".join(style)
+        command.extend([f"{{bolt(10,{text})}}", text, ""])
 
-subprocess.run(command)
+    # Webb-only
+    for style in [["partial"], ["hex", "security"]]:
+        text = ",".join(style)
+        command.extend(["", text, f"{{webbolt({text})}}"])
+
+    subprocess.run(command)
+
+if args.symbols:
+    manifest = fragments.electronic_symbols_manifest()
+    command = [
+        "gflabel",
+        "--base=none",
+        "-w=200",
+        "--height=12",
+        "--divisions=6",
+        "-o",
+        "symbols.svg",
+        *vscode,
+    ]
+    names: dict[str, list[str]] = {}
+    for sym in manifest:
+        names.setdefault(sym["name"], []).append(sym["name"])
+    i = 0
+    for sym in manifest:
+        name = sym["name"]
+        # If we have the same symbol with different standard, say
+        if len(names[name]) > 1:
+            name += f" ({sym['standard']})"
+        command.extend([name, f"{{symbol({sym['id']})}}"])
+        i += 1
+        # if i > 20:
+        #     break
+    print("+ " + " ".join(command))
+    subprocess.run(command)
