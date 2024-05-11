f47b887b45124269a441b7ce7ab2bc05643eccd0
Nicholas Devenish <ndevenish@gmail.com>
2024-05-12 00:22:27 +0100

    README document alignment and columns

--- generate.py
+++ generate.py
@@ -5,24 +5,45 @@ Generate the example layouts for the README
 from __future__ import annotations
 
 import argparse
+import glob
 import subprocess
 
 import gflabel.fragments as fragments
 
 parser = argparse.ArgumentParser(description="Generate example images for GFlabel")
+
+EXAMPLES = ["fragment", "bolt", "drives", "symbols", "columns"]
 parser.add_argument("--vscode", help="Run in OCP_vscode mode", action="store_true")
-parser.add_argument("--fragment", help="Run the fragment examples", action="store_true")
-parser.add_argument("--bolt", help="Run the bolt examples", action="store_true")
-parser.add_argument("--drives", help="Run the drive examples", action="store_true")
-parser.add_argument("--symbols", help="Run the symbol examples", action="store_true")
+
+for kind in EXAMPLES:
+    parser.add_argument(
+        f"--{kind}", help=f"Run the {kind} examples", action="store_true"
+    )
 
 args = parser.parse_args()
 vscode = ["--vscode"] if args.vscode else []
-if all(not x for x in [args.fragment, args.bolt, args.drives, args.symbols]):
-    args.fragment = True
-    args.bolt = True
-    args.drives = True
-    args.symbols = True
+
+if all(not x for x in [getattr(args, name) for name in EXAMPLES]):
+    for name in EXAMPLES:
+        setattr(args, name, True)
+
+
+def gflabel(*args):
+    command = ["gflabel", "-v", *vscode, *args]
+    print("+ " + " ".join(command))
+    subprocess.run(command, check=True)
+
+
+def svgo(*args):
+    cmd = ["svgo"]
+    for arg in args:
+        if "*" in arg:
+            cmd.extend(glob.glob(arg))
+        else:
+            cmd.append(arg)
+    print("+ " + " ".join(cmd))
+    subprocess.run(cmd, check=True)
+
 
 if args.fragment:
     EXAMPLES = [
@@ -33,14 +54,11 @@ if args.fragment:
     HEAD = "gflabel --base=none -w=100 --height=12 --divisions=3 --vscode"
 
     command = [
-        "gflabel",
         "--base=none",
         "-w=100",
         "--height=12",
         "--divisions=3",
-        "-o",
-        "examples.svg",
-        *vscode,
+        "--output=examples.svg",
         "--font-size=7",
     ]
 
@@ -49,7 +67,9 @@ if args.fragment:
             print(frag.names[0], example)
             command.extend([example.replace("{", "{{").replace("}", "}}"), "", example])
 
-    subprocess.run(command)
+    gflabel(*command)
+
+    svgo("examples.svg", "--precision=2")
 
 if args.drives:
     # Generate a table of head drive types
@@ -167,3 +187,40 @@ if args.symbols:
         #     break
     print("+ " + " ".join(command))
     subprocess.run(command)
+
+if args.columns:
+    # ("division", "A\n{measure}" "B\n{measure}" "C\n{measure}"),
+
+    gflabel(
+        "--base=predbox",
+        "--width=5",
+        "--divisions=3",
+        "--box",
+        "--output=column_division.svg",
+        "A\\n{measure}",
+        "B\\n{measure}",
+        "C\\n{measure}",
+    )
+
+    column_examples = [
+        ("basic", "A\n{measure}{|}B\n{measure}{|}C\n{measure}"),
+        (
+            "basic_proportion",
+            "A\n{measure}{4|}B\n{measure}{1|2}C\n{measure}",
+        ),  # "A{4|}B\nC{|}D\nE"),
+        (
+            "basic_proportion_align",
+            "{<}A\n{measure}{4|}{>}B\n{measure}{1|2}{<}C\n{measure}",
+        ),
+    ]
+
+    for name, example in column_examples:
+        gflabel(
+            "--base=predbox",
+            "--width=5",
+            "--box",
+            f"--output=column_{name}.svg",
+            example,
+        )
+
+    svgo("column_*.svg", "--precision=2")
